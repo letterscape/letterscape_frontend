0ca60619026e37cc3a35dcacc25e627eb54c2b29
coco <1285015889@qq.com>
2024-11-30 21:39:04 +0800

    update create page

--- src/components/Create/index.tsx
+++ src/components/Create/index.tsx
@@ -1,15 +1,53 @@
 import { observer } from "mobx-react-lite";
+import { useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
+import { Editor } from '@tinymce/tinymce-react';
 
-interface DraftEditorProps {
-  initialContent?: string; // 可选：初始内容（JSON 格式的字符串）
-  onSave?: (content: string) => void; // 保存事件处理
-}
 
 const Create = () => {
+  const router = useRouter();
+
+  const [content, setContent] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+
+  const handleTitleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTitle(e.target.value)
+  }
+
+  const handleSave = async () => {
+    // 模拟保存到后端的逻辑
+    console.log("content: ", content);
+  };
 
   return (
-    <div>
-      <p>Create</p>
+    <div className="p-4">
+      <div className="flex justify-center items-center">
+        <textarea className="text-center text-5xl border font-bold border-gray-300 rounded border-none outline-none resize-none w-full h-32 p-4" placeholder="Title" onChange={handleTitleChange}>{title}</textarea>
+      </div>
+      <Editor
+        apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
+        value={content}
+        onEditorChange={(newContent) => setContent(newContent)}
+        init={{
+          height: 500,
+          menubar: true,
+          plugins: [
+            'advlist autolink lists link image charmap preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount',
+          ],
+          toolbar:
+            'undo redo | formatselect | bold italic backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             bullist numlist outdent indent | removeformat | help',
+        }}
+      />
+      <button
+        className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+        onClick={handleSave}
+      >
+        Save
+      </button>
     </div>
   );
 }
